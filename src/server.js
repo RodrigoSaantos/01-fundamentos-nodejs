2f19d9a2b44caacc5ec6de2821589c278cb870a1
Rodrigo Santos <rodrigo.silva@gcbinvestimentos.com>
2023-07-13 19:12:26 -0300

    Creating JSON database

--- src/server.js
+++ src/server.js
@@ -1,30 +1,8 @@
 import http from 'node:http'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
+const database = new Database()
 
 const users = []
 
@@ -34,18 +12,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
